68f5876730ed0450262dfe2946a26e79cc014ad6
Lucien <lygm1062@gmail.com>
2019-03-30 18:43:30 +0800

    alpha-v1.1.2

--- routers/router_hoo.js
+++ routers/router_hoo.js
@@ -5,48 +5,48 @@ const Router = require('koa-router');
 const router = new Router();
 
 // 查询账户
-router.post('/api/open/user', async (ctx, next) => {
+router.post('/hoo/api/open/user', async (ctx, next) => {
     await next();
     let params = ctx.request.body;
     ctx.body = await api.checkUser(params);
 });
 
 // 创建商户收款单
-router.post('/api/open/invoices/create', async (ctx, next) => {
+router.post('/hoo/api/open/invoices/create', async (ctx, next) => {
     await next();
     let params = ctx.request.body;
     ctx.body = await api.createInvoices(params);
 });
 
 // 收款单详情
-router.post('/api/open/invoices/detail', async (ctx, next) => {
+router.post('/hoo/api/open/invoices/detail', async (ctx, next) => {
     await next();
     let params = ctx.request.body;
     ctx.body = await api.getInvoices(params);
 });
 
 // 解码收款单
-router.post('/api/open/invoices/decode', async (ctx, next) => {
+router.post('/hoo/api/open/invoices/decode', async (ctx, next) => {
     await next();
     let params = ctx.request.body;
     ctx.body = await api.decodeInvoices(params);
 });
 
 // 向用户支付(前端不能发起向用户的支付)
-// router.post('/api/open/invoices/pay', async (ctx, next) => {
+// router.post('/hoo/api/open/invoices/pay', async (ctx, next) => {
 //     await next();
 //     let params = ctx.request.body;
 //     ctx.body = await api.pay2User(params);
 // });
 
 // 余额查询
-router.post('/api/open/balance', async (ctx, next) => {
+router.post('/hoo/api/open/balance', async (ctx, next) => {
     await next();
     ctx.body = await api.getAppBalance();
 });
 
 // 充值地址
-router.post('/api/open/address', async (ctx, next) => {
+router.post('/hoo/api/open/address', async (ctx, next) => {
     await next();
     ctx.body = await api.getAppAddress();
 });
